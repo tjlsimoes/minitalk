89f9acc8e04c8cac6c2253a180cdac51219f73a5
tjorge-l <tjorge-l@student.42lisboa.com>
2024-10-30 12:22:29 +0000

    Fix norm error

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: tjorge-l <tjorge-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 11:03:39 by tjorge-l          #+#    #+#             */
-/*   Updated: 2024/10/30 11:07:01 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2024/10/30 12:22:24 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	error_check(int k)
 	if (k == -1)
 		exit(1);
 }
+
 void	incorrect_usage(void)
 {
 	ft_putstr_fd("Correct usage: ./client <PID> \"<MESSAGE>\"\n", 2);
